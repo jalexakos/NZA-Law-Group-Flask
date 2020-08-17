f48bf929aed3b4a33ea47ae03b1ff0fb3388f033
snavarrete1 <snavarrete3077@gmail.com>
2020-08-16 19:17:15 -0500

    first

--- NZA_Law/routes.py
+++ NZA_Law/routes.py
@@ -22,12 +22,48 @@ def home():
 
 
 # Retrieve Case route - Stephanie
+@app.route('/cases/<int:case_id>')
+@login_required
+def case_detail(case_id):
+    case = Case.query.get_or_404(case_id)
+    return render_template('case_detail.html', case=case)
+
 
 
 # Updating Case route - Stephanie
+@app.route('/cases/update/<int:case_id>', methods= ['GET', 'POST'])
+@login_required
+def case_update(case_id):
+    case = Case.query.get_or_404(case_id)
+    update_form = CaseNotesForm()
+
+    if request.method == 'POST' and update_form.validate():
+        title = update_form.title.data
+        content = update_form.content.data
+        lawyer_id = current_lawyer.id
+
+        case.title = title
+        case.content = content
+        case.lawyer_id = lawyer_id
+
+
+        db.session.commit()
+        return redirect(url_for('case_update', case_id=case.id))
+    return render_template('case_update.html', update_form=update_form)
+
+
+
+
 
 
 # Deleting Case route - Stephanie
+@app.route('/cases/delete/<int:case_id>', methods=['POST'])
+@login_required
+def case_delete(case_id):
+    case = Case.query.get_or_404(case_id)
+    db.session.delete(case)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 # Login route - Josh
